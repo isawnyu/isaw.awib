e9bfc49d679a19efe6dab08f16d1fc62f5203c05
Tom Elliott <tom.elliott@nyu.edu>
2017-03-03 14:12:00 -0600

    change default fallback profile to the ICC sRGB2014 profile

--- isaw/awib/conversions.py
+++ isaw/awib/conversions.py
@@ -116,7 +116,7 @@ class MasterMaker(Historian):
                 BytesIO(self.original.info['icc_profile']))
         except KeyError:
             raw_profile = self._get_profile_from_file(
-                'sRGB_IEC61966-2-1_black_scaled')
+                'sRGB2014')
             self.log(
                 'Original image does not have an internal ICC color profile.'
                 '{} has been assigned.'
